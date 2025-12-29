d9b4603bbfb62614d2c6aa3d200069f2658746e0
unknown <danilo.bogdanovic@elfak.rs>
2025-12-29 08:35:41 +0100

    update

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,36 @@
-console.log("OVO RADI SVE JE OK")
 
-console.log("OPLA NOVI LOG")
+async function tajmer()
+{
+    return new Promise((resolve, reject) => 
+    {
+        setTimeout(() => 
+        {
+            let br = Math.round(Math.random() * 20)
+            if (br >= 6)
+                resolve(br)
+            else
+                reject("Broj je manji od 5 " + br)
+        }, 1500)    
+    })
+}
 
-console.log("OVO JE NAKON PODESAVANJA TS-A")
 
-console.log("Cetvrti put");
+async function main()
+{
+    try{
+    for(let i = 0; i < 3; i++)
+    {
+        let broj = await tajmer()
+        console.log("Iteracija: "+i+" "+broj )
+    }
+    }
+    catch(e)
+    {
+        console.log(e)
+    }
+
+}
+
+console.log("Ulazim u main!")
+main()
+console.log("Posle maina")
