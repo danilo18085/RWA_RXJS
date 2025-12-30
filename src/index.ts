3af3e4ac04071e94e11fc583ee142e19fb09f6c0
unknown <danilo.bogdanovic@elfak.rs>
2025-12-30 04:57:28 +0100

    obrsani node_modules i dist

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,33 @@
+import {map, take, interval, Subscription, range, Subject, filter, takeUntil} from "rxjs"
 
-async function tajmer()
+
+function pokreni_interval(control$: Subject<any>) : Subscription
 {
-    return new Promise((resolve, reject) => 
+    const tok$: Subscription = interval(500).pipe(
+        filter(x => x % 2 === 0),
+        map(x => x + 1),
+        takeUntil(control$)
+    ).subscribe(x => {console.log("Broj: " + x)})
+
+    return tok$
+}
+
+function make_button1(control$: Subject<any>)
+{
+   const button = document.createElement("button")
+   button.innerHTML = "STOP DUGME"
+   button.onclick = () => 
     {
-        setTimeout(() => 
-        {
-            let br = Math.round(Math.random() * 20)
-            if (br >= 6)
-                resolve(br)
-            else
-                reject("Broj je manji od 5 " + br)
-        }, 1500)    
-    })
+        control$.next(1)
+        control$.complete()
+        console.log("ZAVRSIO SAM SUBSCRIPTION")
+    }
+   document.body.appendChild(button) 
 }
 
 
-async function main()
-{
-    try{
-    for(let i = 0; i < 3; i++)
-    {
-        let broj = await tajmer()
-        console.log("Iteracija: "+i+" "+broj )
-    }
-    }
-    catch(e)
-    {
-        console.log(e)
-    }
+const kontrola_toka$ = new Subject()
 
-}
+make_button1(kontrola_toka$)
+pokreni_interval(kontrola_toka$)
 
-console.log("Ulazim u main!")
-main()
-console.log("Posle maina")
