9d10dc8e2786445b6f072808d7d943faf89c85d8
unknown <danilo.bogdanovic@elfak.rs>
2026-01-09 01:46:04 +0100

    Napravljen kontejner za jednu igricu

--- src/crtanje.ts
+++ src/crtanje.ts
@@ -33,10 +33,54 @@ export function crtaj_gornji_main_div() : void
     const input_bar = document.createElement("input")
     const search_dugme = document.createElement("button")
 
+    input_bar.type = "text"
+    input_bar.placeholder = "Pretrazite igricu..."
+    input_bar.autocomplete = "off"
+
     div_za_search.appendChild(input_bar)
     div_za_search.appendChild(search_dugme)
 
     search_dugme.innerHTML = "Pretraga"
 }
 
+
+export function crtaj_donji_main_div() : void
+{
+    const skup_igrica_kontejner = document.createElement("div")
+    const kasa_div = document.createElement("div")
+
+    skup_igrica_kontejner.className = "skup_igrica_kontejner"
+    skup_igrica_kontejner.id = "skup_igrica_kontejner"
+
+    kasa_div.className = "kasa_div"
+    kasa_div.id = "kasa_div"
+
+    const donji_main_div = document.getElementById("div_donji_main")
+    donji_main_div.appendChild(skup_igrica_kontejner)
+    donji_main_div.appendChild(kasa_div)
+}
+
+
+export function crtaj_kontejner_za_jednu_igricu(naziv_igre : string) : HTMLDivElement
+{
+    const kontejner_igrica = document.createElement("div")
+    kontejner_igrica.className = "kontejner_igrica"
+    kontejner_igrica.id = ("kontejner_igrica_" + naziv_igre)
+
+    kontejner_igrica.innerHTML = `
+     <img src="./src/Slike/FarCry6.png" alt="FarCry6" class="image_wrapper">
+     <p>Zanr: RPG</p>
+     <p>Godina: 2021</p>
+     <p>Rating: 9.8</p>
+     <div class="divider_linija"></div>
+     
+     <div class="cena_kontejner">
+     <h3>Cena 30.12$</h3>
+     <h3>-15%</h3>
+     </div>
+      
+     <button>KUPI</button>
+    `
+    return kontejner_igrica
+}
 
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
-import { crtaj_gornji_main_div, postavi_dva_glavna_diva } from "./crtanje";
+import { crtaj_donji_main_div, crtaj_gornji_main_div, crtaj_kontejner_za_jednu_igricu, postavi_dva_glavna_diva } from "./crtanje";
 
-postavi_dva_glavna_diva()
-crtaj_gornji_main_div()
+//postavi_dva_glavna_diva()
+//crtaj_gornji_main_div()
+//crtaj_donji_main_div()
+
+document.body.appendChild(crtaj_kontejner_za_jednu_igricu("FarCry6"))
